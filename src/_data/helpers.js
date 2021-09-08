908856af5d98d02df3ea60c58742efc971cf39fd
roberto3003 <roberto3003@riseup.net>
2021-09-08 21:17:37 +0700

    lesson 13

--- src/_data/helpers.js
+++ src/_data/helpers.js
@@ -8,16 +8,53 @@ module.exports = {
    * @returns {String} The attributes or empty
    */
   getLinkActiveState(itemUrl, pageUrl) {
-    let response = '';
+	let response = '';
 
-    if (itemUrl === pageUrl) {
-      response = ' aria-current="page"';
-    }
+	if (itemUrl === pageUrl) {
+	  response = ' aria-current="page"';
+	}
 
-    if (itemUrl.length > 1 && pageUrl.indexOf(itemUrl) === 0) {
-      response += ' data-state="active"';
-    }
+	if (itemUrl.length > 1 && pageUrl.indexOf(itemUrl) === 0) {
+	  response += ' data-state="active"';
+	}
 
-    return response;
+	return response;
+  },
+  /**
+   * Filters out the passed item from the passed collection
+   * and randomises and limits them based on flags
+   *
+   * @param {Array} collection The 11ty collection we want to take from
+   * @param {Object} item The item we want to exclude (often current page)
+   * @param {Number} limit=3 How many items we want back
+   * @param {Boolean} random=true Wether or not this should be randomised
+   * @returns {Array} The resulting collection
+   */
+  getSiblingContent(collection, item, limit = 3, random = true) {
+	let filteredItems = collection.filter(x => x.url !== item.url);
+
+	if (random) {
+	  let counter = filteredItems.length;
+
+	  while (counter > 0) {
+		// Pick a random index
+		let index = Math.floor(Math.random() * counter);
+
+		counter--;
+
+		let temp = filteredItems[counter];
+
+		// Swap the last element with the random one
+		filteredItems[counter] = filteredItems[index];
+		filteredItems[index] = temp;
+	  }
+	}
+
+	// Lastly, trim to length
+	if (limit > 0) {
+	  filteredItems = filteredItems.slice(0, limit);
+	}
+
+	return filteredItems;
   }
 };
