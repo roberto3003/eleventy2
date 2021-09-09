201f8ec7f2bc653ee29fc30bd9991e02e6bc068d
roberto3003 <roberto3003@riseup.net>
2021-09-09 17:26:37 +0700

    lesson 22

--- gulpfile.js
+++ gulpfile.js
@@ -2,15 +2,16 @@ const {parallel, watch} = require('gulp');
 
 // Pull in each task
 const fonts = require('./gulp-tasks/fonts.js');
-const sass = require('./gulp-tasks/sass.js');
 const images = require('./gulp-tasks/images.js');
+const sass = require('./gulp-tasks/sass.js');
+
 // Set each directory and contents that we want to watch and
 // assign the relevant task. `ignoreInitial` set to true will
 // prevent the task being run when we run `gulp watch`, but it
 // will run when a file changes.
 const watcher = () => {
-  watch('./src/scss/**/*.scss', {ignoreInitial: true}, sass);
   watch('./src/images/**/*', {ignoreInitial: true}, images);
+  watch('./src/scss/**/*.scss', {ignoreInitial: true}, sass);
 };
 
 // The default (if someone just runs `gulp`) is to run each task in parrallel
